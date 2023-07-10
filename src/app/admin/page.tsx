a90e67345aef05db4d778e98f6f54534946c2cde
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-07-10 14:21:44 -0400

    admin ui update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,9 +3,8 @@ import Admin from "@/app/components/Admin";
 
 // import { currentUser } from "@clerk/nextjs/app-beta";
 const page = () => {
-
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center md:max-w-4xl mx-auto">
+    <div className="flex pt-12 w-full flex-col items-center justify-center md:max-w-4xl mx-auto">
       <Admin />
     </div>
   );
