dcdc9dba6cf8e6a7b3cb4c00d145f18d4cd06bf4
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-07-11 14:28:03 -0400

    i can now delete from admin area

---
+++ src/app/utils/deleteWordsFromDb.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/app/utils/prismaClient";
+
+export async function deleteWordsFromDb(words: string[]): Promise<{
+  deletedWordsCount: number;
+  skippedWordsCount: number;
+}> {
+  let deletedWordsCount = 0;
+  let skippedWordsCount = 0;
+
+  for (const word of words) {
+    try {
+      const existingWord = await prisma.badWordsList.findUnique({
+        where: {
+          word: word.trim(),
+        },
+      });
+
+      if (!existingWord) {
+        skippedWordsCount++;
+        continue; // Skip the current word and move to the next one
+      }
+
+      await prisma.badWordsList.delete({
+        where: {
+          word: word,
+        },
+      });
+
+      deletedWordsCount++;
+    } catch (error) {
+      console.error(`Error deleting word '${word}': ${error as string}`);
+    }
+  }
+
+  console.log(`Deleted words: ${deletedWordsCount}`);
+  console.log(`Skipped words: ${skippedWordsCount}`);
+  return { deletedWordsCount, skippedWordsCount };
+}
