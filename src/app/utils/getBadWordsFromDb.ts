334243298dd811b397c2f690c36a396e6b43cead
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-07-20 13:43:37 -0400

    that fu issues might be fixed, testing

--- src/app/utils/getBadWordsFromDb.ts
+++ src/app/utils/getBadWordsFromDb.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/app/utils/prismaClient";
 export const getBadWordsFromDb = async () => {
   const badWordsFromDb = await prisma.badWordsList.findMany();
-  let badWordsList = badWordsFromDb.map((word) => word.word);
+  let badWordsList = badWordsFromDb.map((word:any) => word.word);
   return badWordsList;
 };
