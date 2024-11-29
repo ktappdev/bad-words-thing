848d13dd9d5cfb6822f208dbbd2f21586951ab91
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-11-29 13:17:35 -0400

    fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Admin from "@/app/components/Admin";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-// import { currentUser } from "@clerk/nextjs/app-beta";
-const page = () => {
+const page = async () => {
+  const { userId } = auth();
+  
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="flex pt-12 w-full flex-col items-center justify-center md:max-w-4xl mx-auto">
       <Admin />
